a948cef1665505f0b30a553ada90b90e3fce2dbb
Jason Montalto <jason@montydigital.com>
2014-05-20 21:43:53 -0500

    Update main.js

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,20 +1,22 @@
 'use strict';
 
-angular.module('chuteAngularApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma',
-      'chute'
-    ];
-  });
+angular.module('chuteAngularApp').
+    controller('MainCtrl', ['$scope', 'Chute.API.Asset',
+        function($scope, Asset) {
 
-// Chicago Life       = ayg4tmuc/2514972
-// Chicago Landmarks  = ayg5ofmi/2514973
-// Chicago Food       = ayg6yqbu/2514974
-// Other Chicago      = ayg7ydjb/2514975
 
-angular.module('chuteAngularApp').controller('MainCtrl', ['$scope', 'Chute.API.Asset', function($scope, Asset) {
-  $scope.assets = Asset.query({album: 'ayg5ofmi', perPage: 3});
-}]);
+            //https://api.getchute.com/v2/albums/ayg4tmuc/assets
+
+            // Chicago Life       = ayg4tmuc/2514972
+            // Chicago Landmarks  = ayg5ofmi/2514973
+            // Chicago Food       = ayg6yqbu/2514974
+            // Other Chicago      = ayg7ydjb/2514975
+
+            $scope.assets = Asset.query({
+                album: 'ayg5ofmi',
+                perPage: 3
+            });
+
+
+        }
+    ]);
